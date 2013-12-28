b7e3870d4a53fd04922d2c874d0b230a31c4ff58
Louis Wilbrink Air <louis.wilbrink@gmail.com>
2013-12-28 17:26:27 -0500

    Refactor scene from renderer directive to renderer service

--- app/scripts/directives/renderer.js
+++ app/scripts/directives/renderer.js
@@ -7,69 +7,42 @@ angular.module('angularWebGL')
       controller: function controller($scope, $element, $attrs, rendererSvc) {
 
 
-        /*** MODEL ***/
+        // MODEL
 
         var container = $element[0];
-        var aspectRatio = container.offsetWidth / container.offsetHeight;
-
-        rendererSvc.init(aspectRatio);
-
-        var renderer = new THREE.WebGLRenderer({ antialias: true });
+        var renderer = rendererSvc.init(container);
         var camera = rendererSvc.getCamera();
-
-        console.log(camera);
-        var scene = new THREE.Scene();
+        var scene = rendererSvc.getScene();
 
         $scope.cameraPositionZ = 3;
 
-        /*** METHODS ***/
+        // METHODS
 
         var run = function () {
           // Render the scene
           renderer.render( scene, camera );
 
-          cube.rotation.y = Date.now() * 0.0005;
+          // cube.rotation.y = Date.now() * 0.0005;
 
           // Ask for another frame
           requestAnimationFrame(run);	
         }
 
-        /*** INITIALIZATION ***/
+        // INITIALIZATION
 
-        renderer.setSize(container.offsetWidth, container.offsetHeight);
+        container.appendChild(renderer.domElement);
 
-        var canvas = renderer.domElement;
+        run();
 
-        container.appendChild(canvas);
+        // EVENT-HANDLERS
 
         $scope.$watch('cameraPositionZ', function (newValue, oldValue) {
           if (newValue) {
-            camera.position.set(0, 0, $scope.cameraPositionZ);
+            console.log(rendererSvc.getCamera().position);
+            camera.position.set(0, 0, parseInt($scope.cameraPositionZ));
+            console.log(rendererSvc.getCamera().position);
           }
         });
-
-        // Create a directional light to show off the object
-        var light = new THREE.DirectionalLight( 0xffffff, 1.5);
-        light.position.set(0, 0, 1);
-        scene.add(light);
-
-        // Now, create a Phong material to show shading; pass in the map
-        var material = new THREE.MeshPhongMaterial();
-
-        // Create the cube geometry
-        var geometry = new THREE.CubeGeometry(1, 1, 1);
-
-        // And put the geometry and material together into a mesh
-        var cube = new THREE.Mesh(geometry, material);
-
-        // Turn it toward the scene, or we won't see the cube shape!
-        cube.rotation.x = Math.PI / 5;
-        cube.rotation.y = Math.PI / 5;
-
-        // Add the cube to our scene
-        scene.add(cube);
-
-        run();
       }
     };
   });
--- app/scripts/services/rendererSvc.js
+++ app/scripts/services/rendererSvc.js
@@ -3,27 +3,75 @@
 angular.module('angularWebGL')
   .factory('rendererSvc', function () {
 
-    var camera = null; // PerspectiveCamera
+    // MODEL
+
+    var renderer,
+        camera,
+        light,
+        scene;
+
     var meshes = [];
 
-    // CONST
-    var fieldOfVision = 45;
+    // METHODS
 
-    var init = function (aspectRatio) {
+    var createCubes = function () {
 
-      camera = new THREE.PerspectiveCamera(fieldOfVision, aspectRatio, 1, 4000);
+      // Now, create a Phong material to show shading; pass in the map
+      var material = new THREE.MeshPhongMaterial();
+
+      // Create the cube geometry
+      var geometry = new THREE.CubeGeometry(1, 1, 1);
+
+      // And put the geometry and material together into a mesh
+      var cube = new THREE.Mesh(geometry, material);
+
+      // Turn it toward the scene, or we won't see the cube shape!
+      cube.rotation.x = Math.PI / 5;
+      cube.rotation.y = Math.PI / 5;
+
+      // Add the cube to our scene
+      scene.add(cube);
+    };
+
+    var init = function (container) {
+
+      // Create renderer and set size according to parent size.
+      renderer = new THREE.WebGLRenderer({ antialias: true });
+
+      renderer.setSize(container.offsetWidth, container.offsetHeight);
+
+      // Create the scene.
+      scene = new THREE.Scene();
+
+      // Create and set initial position of camera.
+      camera = new THREE.PerspectiveCamera(45, container.offsetWidth / container.offsetHeight, 1, 4000);
 
       camera.position.set( 0, 0, 3);
 
+      // Create and position directional light and add to scene.
+      light = new THREE.DirectionalLight( 0xffffff, 1.5);
+      light.position.set(0, 0, 1);
+
+      scene.add(light);
+
+      createCubes();
+
+      return renderer;
     };
 
-    // Public API here
+    // API
+
     return {
       init: init,
 
       getCamera: function () {
 
         return camera;
+      },
+
+      getScene: function () {
+
+        return scene;
       }
     };
   });
